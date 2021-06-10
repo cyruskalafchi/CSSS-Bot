ff3dfe08d1d845735425414fd45a4eaebf214139
Cyrus Kalafchi <cyrus.kalafchi@gmail.com>
2021-06-10 16:42:40 -0700

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,18 @@
+# Discord Bot for UBC CSSS' Server
+# Note: Code is originally based on RealPython's Discord Bot guide
+# bot.py
+
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
